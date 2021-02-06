5224d6ebef43d07e431b010c8f9e86e143f6cefb
Joe Moore <joe.moore@imperva.com>
2021-02-06 10:43:30 -0800

    Updating variables, removing the type and defaults.

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
 variable "account_id" {default = 2398}
-variable "countries" {
-  type = list(string)
-  default = []
-}
-variable "ips" {
-  type = list(string)
-  default = []
-}
+variable "countries" {}
+variable "ips" {}
